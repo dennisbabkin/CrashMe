a469eac1a58863ec2e5e9b0da2e495c9f830fc1f
dennisbabkin <dennis_babkin@hotmail.com>
2023-06-21 11:08:54 +0300

    Initial Commit

---
+++ CrashMe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CrashMe
+//
+//  Created by Denn1s on 6/21/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
